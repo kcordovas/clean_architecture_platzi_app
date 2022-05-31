2b77d879b66c5f35b54be3c97c3adaa24a1a846d
Kevin Cordova <kevin.cordova@bayteq.com>
2022-05-31 11:49:13 -0500

    Data layer in new module: with Remote repository for Character

---
+++ app/src/main/kotlin/com/platzi/android/rickandmorty/api/ApiDataSources.kt
@@ -0,0 +1,19 @@
+package com.platzi.android.rickandmorty.api
+
+import com.platzi.android.rickandmorty.data.RemoteCharacterDataSource
+import com.platzi.android.rickandmorty.domain.Character
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class CharacterRetrofitDataSource(
+    private val characterRequest: CharacterRequest
+) : RemoteCharacterDataSource {
+    override fun getAllCharacters(page: Int): Single<List<Character>> =
+        characterRequest
+            .getService<CharacterService>()
+            .getAllCharacters(page)
+            .map(CharacterResponseServer::toCharacterDomainList)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+}
--- app/src/main/kotlin/com/platzi/android/rickandmorty/usescases/GetAllCharactersUseCase.kt
+++ app/src/main/kotlin/com/platzi/android/rickandmorty/usescases/GetAllCharactersUseCase.kt
@@ -1,18 +1,10 @@
 package com.platzi.android.rickandmorty.usescases
 
-import com.platzi.android.rickandmorty.api.*
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.platzi.android.rickandmorty.data.CharacterRepository
 
 class GetAllCharactersUseCase(
-    private val characterRequest: CharacterRequest
+    private val characterRepository: CharacterRepository
 ) {
 
-    fun invoke(currentPage: Int) =
-        characterRequest
-            .getService<CharacterService>()
-            .getAllCharacters(currentPage)
-            .map(CharacterResponseServer::toCharacterDomainList)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
+    fun invoke(currentPage: Int) = characterRepository.getAllCharacters(currentPage)
 }
